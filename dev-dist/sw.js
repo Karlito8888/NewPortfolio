b660d4ad6c65e8ff9a70288de40026583a07617d
Karlito8888 <charles.bourgault@hotmail.fr>
2024-10-20 14:08:56 +0200

    MaJ CV, et suppression du manifest.json

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-95dee3cf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.vh1grj3di7g"
+    "revision": "0.ckglbc6ilio"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
