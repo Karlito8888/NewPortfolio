dda3e849faaae02e99242f0028a14159b35b4c9f
Karlito8888 <charles.bourgault@hotmail.fr>
2024-10-21 11:29:32 +0200

    Ajouts Animations svg

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-95dee3cf'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.d3t7pssnkho"
+    "revision": "0.bbg9jm8vuqg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
