2dca8b68ad16f6275a860252c3ce9dff7fa67a54
Karlito8888 <charles.bourgault@hotmail.fr>
2024-11-20 12:39:37 +0100

    Ajustement robots.txt

--- vite.config.js
+++ vite.config.js
@@ -11,6 +11,7 @@ export default defineConfig({
       registerType: "autoUpdate",
       includeAssets: [
         "favicon.ico", 
+        "robots.txt",
         "apple-touch-icon.png",
         "icons/*.png"
       ],
